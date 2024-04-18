bd8dd4138dd4129a20489b71e19ecc39139fe94f
herdeybayor <shereefdeen4real@gmail.com>
2024-04-18 12:11:30 +0100

    chore: update AndroidManifest.xml and home page files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonImg,
+  IonGrid,
+  IonRow,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonRow,
+    IonGrid,
+    IonImg,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+  ],
 })
 export class HomePage {
   constructor() {}
