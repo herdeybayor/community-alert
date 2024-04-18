f143c007fa20f7e618e1243461f61c10f2c5e33f
herdeybayor <shereefdeen4real@gmail.com>
2024-04-18 14:17:44 +0100

    Add new services and update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDLidJjGs5l9vClGSMAfQ4j4-FPq9p1FA8',
+    authDomain: 'community-alert-d3fad.firebaseapp.com',
+    projectId: 'community-alert-d3fad',
+    storageBucket: 'community-alert-d3fad.appspot.com',
+    messagingSenderId: '1054687387762',
+    appId: '1:1054687387762:web:b85b6617fbb1c4c5bbe2eb',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDLidJjGs5l9vClGSMAfQ4j4-FPq9p1FA8',
+    authDomain: 'community-alert-d3fad.firebaseapp.com',
+    projectId: 'community-alert-d3fad',
+    storageBucket: 'community-alert-d3fad.appspot.com',
+    messagingSenderId: '1054687387762',
+    appId: '1:1054687387762:web:b85b6617fbb1c4c5bbe2eb',
+  },
 };
 
 /*
